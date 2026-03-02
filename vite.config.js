0ad80d31fd857089f9a7493af412ef05173c83cf
npsatria <dewangga.satria09@gmail.com>
2026-03-03 06:27:10 +0800

    Refactor: Move HTML pages to src/pages directory

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 
+import { resolve } from 'path';
+
 export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        news: resolve(__dirname, 'src/pages/news.html'),
+        stocks: resolve(__dirname, 'src/pages/stocks.html'),
+      },
+    },
+  },
 });
